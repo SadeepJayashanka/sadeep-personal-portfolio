a78088765cd223ba99d24df0f6196c20a66804d0
Sadeep Jayashanka <jayashankasad@gmail.com>
2025-09-30 13:07:00 +0530

    feat: implement Header component and integrate Tailwind CSS scrollbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+/**
+ * @copyright Sadeep Jayashanka
+ * @license Apache-2.0
+ */
+
+const Header = () => {
+  return (
+    <header
+      className="fixed top-0 left-0 w-full h-20 flex 
+        items-center z-40 bg-gradient-to-b from-zinc-900to-zinc-900/0"
+    >
+      {" "}
+    </header>
+  );
+};
+export default Header;
