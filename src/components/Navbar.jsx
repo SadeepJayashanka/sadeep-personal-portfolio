6e59bb1a4e6bf81db5b3e30320271c284a4c4620
Sadeep Jayashanka <jayashankasad@gmail.com>
2025-10-04 11:37:19 +0530

    feat: enhance Navbar active link functionality and improve responsive styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,10 +14,30 @@ const Navbar = ({ navOpen }) => {
   const activeBox = useRef();
 
   const initActiveBox = () => {
-    console.log(lastActiveLink.current);
+    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
+
+    activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
+
+    activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
+
+    activeBox.current.style.height = lastActiveLink.current.offsetHeight + "px";
   };
 
   useEffect(initActiveBox, []);
+
+  const activeCurrentLink = (event) => {
+    lastActiveLink.current?.classList.remove("active");
+    event.target.classList.add("active");
+    lastActiveLink.current = event.target;
+
+    activeBox.current.style.top = event.target.current.offsetTop + "px";
+
+    activeBox.current.style.left = event.target.current.offsetLeft + "px";
+
+    activeBox.current.style.width = event.target.current.offsetWidth + "px";
+
+    activeBox.current.style.height = event.target.current.offsetHeight + "px";
+  };
 
   const navItems = [
     {
@@ -50,7 +70,13 @@ const Navbar = ({ navOpen }) => {
   return (
     <nav className={`navbar ${navOpen ? "active" : ""}`}>
       {navItems.map(({ label, link, className, ref }, key) => (
-        <a href={link} key={key} ref={ref} className={className} onClick={null}>
+        <a
+          href={link}
+          key={key}
+          ref={ref}
+          className={className}
+          onClick={activeCurrentLink}
+        >
           {label}
         </a>
       ))}
