6696f72a16aa023fb4fe591b88edb905ed310112
Sadeep Jayashanka <jayashankasad@gmail.com>
2025-10-10 18:10:49 +0530

    feat: enhance Hero component layout and styling for improved presentation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,23 +7,34 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section id="Home" className="">
-      <div className="">
+    <section id="Home" className="pt-28 lg:pt-36">
+      <div
+        className="container items-center lg:grid lg:grid-cols-2
+       lg:gap-10"
+      >
         <div>
-          <div className="">
-            <figure className="">
+          <div className="flex items-center gap-3">
+            <figure className="img-box w-9 h-9 rounded-lg ">
               <img
                 src="/images/avatar-1.jpg"
                 width={40}
                 height={40}
                 alt="Sadeep Jayashanka protrait"
-                l
                 className="img-cover"
               />
             </figure>
-            <div className="">
-              <span className="">
-                <span className=""></span>
+            <div
+              className="flex items-center gap-1.5
+            text-zinc-400 text-sm tracking-wide"
+            >
+              <span
+                className="relative w-2 h-2 
+              rounded-full bg-emerald-400"
+              >
+                <span
+                  className="absolute inset-0
+                 rounded-full bg-emerald-400 animate-ping"
+                ></span>
               </span>
               Available for work
             </div>
