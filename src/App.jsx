3529e5ca35403592774d34ac499b4c23e5f6d7ff
Sadeep Jayashanka <jayashankasad@gmail.com>
2025-10-20 22:03:32 +0530

    feat: add Contact component for user collaboration inquiries

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Review from "./components/Review";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Skill />
         <Work />
         <Review />
+        <Contact />
       </main>
     </>
   );
